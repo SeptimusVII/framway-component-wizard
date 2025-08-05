b60c58084392680c7dd4acb52a4c5b7a67031bca
SeptimusVII <vansteene.quentin@gmail.com>
2025-08-05 17:30:24 +0200

    fix input-radius usage

--- wizard.js
+++ wizard.js
@@ -2,8 +2,8 @@ module.exports = function(app){
     var Wizard = Object.getPrototypeOf(app).Wizard = new app.Component("wizard");
     // Wizard.debug = true;
     Wizard.createdAt      = "2.0.0";
-    Wizard.lastUpdate     = "2.0.0";
-    Wizard.version        = "1";
+    Wizard.lastUpdate     = "2.6.0";
+    Wizard.version        = "1.0.1";
     // Wizard.factoryExclude = true;
     // Wizard.loadingMsg     = utils.renderError === undefined ? "utils.renderError function is not defined" : false;
     // Wizard.requires       = [];
